a7a137c47e7c03caea7f7eab243d03efd46f60ac
Mathieu Maisonnette <mathieu.maisonnette@gmail.com>
2025-03-24 01:06:10 +0100

    Comments in Python, cleaning of the folder
    Reinstatement of the model export deleted in a previous commit

--- python_to_send_data.py
+++ python_to_send_data.py
@@ -1,16 +1,13 @@
 import serial
 import numpy as np
 
-PORT = "COM21"
-
+PORT = "COM12" # Change this to the port where the STM32 is connected
 
 def synchronise_UART(serial_port):
     """
     Synchronizes the UART communication by sending a byte and waiting for a response.
-
     Args:
         serial_port (serial.Serial): The serial port object to use for communication.
-
     Returns:
         None
     """
@@ -40,31 +37,28 @@ def send_inputs_to_STM32(inputs, serial_port):
     for x in inputs:
         buffer += x.tobytes()
     serial_port.write(buffer)
-    print(buffer)
 
 
 def read_output_from_STM32(serial_port):
     """
-    Reads 4 bytes from the given serial port and reconstructs a float value.
+    Reads only 1 bytes from the given serial port and recuperate directly a float value. The interest to use a int instead
+    of a float is to save transmission time and to be able to use the int value directly from the STM32 without any reconstruction.
 
     Args:
     serial_port: A serial port object.
 
     Returns:
-    The reconstructed float value.
+    The value.
     """
-    output = serial_port.read(1)  # Un uint8_t est sur 1 Un float IEEE 754 est codé sur 4 octets
+    output = serial_port.read(1)  # Un uint8_t est sur 1
 
     if len(output) != 1:
-        print(len(output))
-        raise ValueError("Erreur de réception : 1 octet attendus")
-
-    #print("Vrai output :", output)
-    #print("Octets reçus (hex) :", output.hex())  # Affiche en hexadécimal
+        raise ValueError("Erreur de réception : 1 octet attendu mais {} reçu(s).".format(len(output)))
 
     return output[0]
 
-def analyse_output(output):
+#The output of the card is analyzed to show the user the type of failure predicted (or to say that there is no problem).
+def analyse_output(output): 
     if output == 0:
         return "No failure"
     elif output == 1:
@@ -90,10 +84,8 @@ def evaluate_model_on_STM32(iterations, serial_port):
         float: The accuracy of the model, as a percentage.
     """
     accuracy = 0
-    for i in range(iterations):
-       # print(Y_test[i])
+    for i in range(iterations):#The number of iterations is defined by the user in the main function
         print(f"----- Iteration {i+1} -----")
-        print(X_test[i])
         send_inputs_to_STM32(X_test[i], serial_port)
         output = read_output_from_STM32(serial_port)
         if (output == Y_test[i]):
@@ -107,15 +99,12 @@ def evaluate_model_on_STM32(iterations, serial_port):
 
     return accuracy
 
-
+# Main function
 if __name__ == '__main__':
 
-    X_test = np.load("./saved_data/X_test.npy") # Changer par X.npy si on veut tester les données brutes (équilibrées)
-    Y_test = np.load("./saved_data/Y_test.npy") # Change par Y.npy si on veut tester les données brutes (équilibrées)
-    print(X_test[7])
-    print(X_test.shape)
-    print(Y_test.shape)
-    print(Y_test[7])
+     #The data is loaded from the saved_data folder
+    X_test = np.load("./saved_data/X_test.npy") # Change to X.npy if you want to test the raw (balanced) data
+    Y_test = np.load("./saved_data/Y_test.npy") # Change to Y.npy if you want to test the raw (balanced) data
 
     with serial.Serial(PORT, 115200, timeout=1) as ser:
         print("Synchronising...")
@@ -123,4 +112,4 @@ if __name__ == '__main__':
         print("Synchronised")
 
         print("Evaluating model on STM32...")
-        error = evaluate_model_on_STM32(100, ser)
+        error = evaluate_model_on_STM32(500, ser)#The number of iterations is defined by the user here
